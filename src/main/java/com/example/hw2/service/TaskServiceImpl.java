45fb677b9002e4801acfa740a3dabfc8f9793b41
Mikhail Grachev <karlson-2002@bk.ru>
2024-04-17 13:10:24 +0800

    add transactional

--- src/main/java/com/example/hw2/service/TaskServiceImpl.java
+++ src/main/java/com/example/hw2/service/TaskServiceImpl.java
@@ -4,7 +4,9 @@ import com.example.hw2.model.Task;
 import com.example.hw2.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,14 +23,17 @@ public class TaskServiceImpl implements TaskService{
         return taskRepository.findById(id);
     }
 
+    @Transactional(rollbackFor = { SQLException.class })
     public void createTask(Task task) {
         taskRepository.save(task);
     }
 
+    @Transactional(rollbackFor = { SQLException.class })
     public void deleteTask(Long id) {
         taskRepository.deleteById(id);
     }
 
+    @Transactional(rollbackFor = { SQLException.class })
     public void updateTask(Task task, Long taskId) {
         taskRepository.findById(taskId).ifPresent(tmpTask -> {
             mapping(task, tmpTask);
